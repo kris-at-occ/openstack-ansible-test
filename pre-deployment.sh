8473e9b3f4d4bc21b6ab82fbe4350b7a0283dac0
Kris Celmer <35005462+kris-at-occ@users.noreply.github.com>
2018-07-06 22:04:03 +0200

    Update pre-deployment.sh

--- pre-deployment.sh
+++ pre-deployment.sh
@@ -47,3 +47,5 @@ echo 'run-kolla.sh: Running ssh openstack@compute1 "sudo bash /home/openstack/co
 ssh openstack@compute1 "sudo bash /home/openstack/compute1.setup.sh"
 echo 'run-kolla.sh: Running ssh openstack@storage1 “sudo bash /home/openstack/storage1.setup.sh”'
 ssh openstack@storage1 “sudo bash /home/openstack/storage1.setup.sh”
+
+sudo cp -r /opt/openstack-ansible/etc/openstack_deploy/* /etc/openstack_deploy
