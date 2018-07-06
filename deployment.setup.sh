5e428671c46f18d3b4df391ec1a0ec4854e33b80
Kris Celmer <35005462+kris-at-occ@users.noreply.github.com>
2018-07-06 22:09:44 +0200

    Update deployment.setup.sh

--- deployment.setup.sh
+++ deployment.setup.sh
@@ -6,7 +6,7 @@ export LANG="en_US.UTF-8"
 
 DEBIAN_FRONTEND=noninteractive apt-get update -y
 DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y
-DEBIAN_FRONTEND=noninteractive apt-get install aptitude build-essential ntp ntpdate openssh-server python-dev
+DEBIAN_FRONTEND=noninteractive apt-get -y install aptitude build-essential ntp ntpdate openssh-server python-dev
 reboot
 
 sudo git clone -b 17.0.5 https://git.openstack.org/openstack/openstack-ansible /opt/openstack-ansible
