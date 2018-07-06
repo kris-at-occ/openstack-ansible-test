b19433665ca4222eb96ad54e86e3d5c9211e0ef8
Kris Celmer <35005462+kris-at-occ@users.noreply.github.com>
2018-07-06 22:22:38 +0200

    Update storage1.setup.sh

--- storage1.setup.sh
+++ storage1.setup.sh
@@ -7,6 +7,6 @@ export LANG="en_US.UTF-8"
 DEBIAN_FRONTEND=noninteractive apt-get update -y
 DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y 
 
-DEBIAN_FRONTEND=noninteractive apt-get install bridge-utils debootstrap ifenslave ifenslave-2.6 lsof lvm2 ntp ntpdate openssh-server sudo tcpdump vlan python
+DEBIAN_FRONTEND=noninteractive apt-get install -y bridge-utils debootstrap ifenslave ifenslave-2.6 lsof lvm2 ntp ntpdate openssh-server sudo tcpdump vlan python
 
 cp /home/openstack/storage1.interfaces /etc/network/interfaces
