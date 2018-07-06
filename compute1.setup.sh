2b6b82a0ca37687a904a855bd8805f5c9e1e7f9e
Kris Celmer <35005462+kris-at-occ@users.noreply.github.com>
2018-07-06 20:08:28 +0200

    Create compute1.setup.sh

---
+++ compute1.setup.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+export LC_ALL=C
+export LC_CTYPE="UTF-8",
+export LANG="en_US.UTF-8"
+
+DEBIAN_FRONTEND=noninteractive apt-get update -y
+DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y 
+
+DEBIAN_FRONTEND=noninteractive apt-get install bridge-utils debootstrap ifenslave ifenslave-2.6 lsof lvm2 ntp ntpdate openssh-server sudo tcpdump vlan python
+
