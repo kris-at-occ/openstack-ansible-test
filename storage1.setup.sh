27f950980dfba683e55ec203dc0926aa9229f3f7
Kris Celmer <35005462+kris-at-occ@users.noreply.github.com>
2018-07-07 12:22:36 +0200

    Update storage1.setup.sh

--- storage1.setup.sh
+++ storage1.setup.sh
@@ -9,10 +9,14 @@ export LANG="en_US.UTF-8"
 mkdir /root/.ssh
 cp /home/openstack/.ssh/authorized_keys /root/.ssh/authorized_keys
 
+# Install packages, as required in https://docs.openstack.org/project-deploy-guide/openstack-ansible/queens/targethosts.html#configuring-the-operating-system
+
 DEBIAN_FRONTEND=noninteractive apt-get update -y
 DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y 
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y bridge-utils debootstrap ifenslave ifenslave-2.6 lsof lvm2 ntp ntpdate openssh-server sudo tcpdump vlan python
+
+# Create /etc/network/interfaces, as required in https://docs.openstack.org/openstack-ansible/queens/user/test/example.html#host-network-configuration
 
 cp /home/openstack/storage1.interfaces /etc/network/interfaces
 
