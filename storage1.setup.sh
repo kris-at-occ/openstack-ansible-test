b4fc507be694f92b53e7c38061bd21a8bb708c96
Kris Celmer <35005462+kris-at-occ@users.noreply.github.com>
2018-07-07 12:13:40 +0200

    Update storage1.setup.sh

--- storage1.setup.sh
+++ storage1.setup.sh
@@ -4,6 +4,11 @@ export LC_ALL=C
 export LC_CTYPE="UTF-8",
 export LANG="en_US.UTF-8"
 
+# Set-up SSH keys, as required in https://docs.openstack.org/project-deploy-guide/openstack-ansible/queens/targethosts.html#configure-ssh-keys
+
+mkdir /root/.ssh
+cp /home/openstack/.ssh/authorized_keys /root/.ssh/authorized_keys
+
 DEBIAN_FRONTEND=noninteractive apt-get update -y
 DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y 
 
