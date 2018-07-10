1b763af1ba3569b6602899adcec7fb488c1be560
Kris Celmer <35005462+kris-at-occ@users.noreply.github.com>
2018-07-10 16:19:35 +0200

    Update run-osa.sh

--- run-osa.sh
+++ run-osa.sh
@@ -4,6 +4,12 @@ export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 
+
+sudo git clone -b 17.0.5 https://git.openstack.org/openstack/openstack-ansible /opt/openstack-ansible
+
+cd /opt/openstack-ansible
+sudo scripts/bootstrap-ansible.sh
+
 cd /opt/openstack-ansible/playbooks
 
 echo "Running sudo openstack-ansible setup-everything.yml"
