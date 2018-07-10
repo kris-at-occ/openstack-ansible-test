f30026dab0dc6a8e3d847bbf23c897d059fb5aab
Kris Celmer <35005462+kris-at-occ@users.noreply.github.com>
2018-07-10 08:51:22 +0200

    Update target.setup.sh

--- target.setup.sh
+++ target.setup.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-export LC_ALL=C
-export LC_CTYPE="UTF-8",
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
 export LANG="en_US.UTF-8"
 
 # Set-up SSH keys, as required in https://docs.openstack.org/project-deploy-guide/openstack-ansible/queens/targethosts.html#configure-ssh-keys
